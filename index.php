f05c4f82e4414cff69d2ab6f286c136ba701df43
Cedric Van Pelt <cvp@mobilemyday.com>
2017-08-02 15:18:59 +0200

    test

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <?php
+	error_reporting(-1);
+ini_set('display_errors', 'On');
+set_error_handler("var_dump");
+
 	$success = mail(
 		'cvp@mobilemyday.com', 
 		'[MMD] test', 
 		'Test' . '<br />' . '<pre>' . 'test2' . '</pre>', 
 		'FROM: mobile my day <test@mobilemyday.com>' . "\n" . 'Content-Type: text/html; charset="utf-8"' . "\n" 
 	);
-if (!$success) {
-    echo("ERROR: ".error_get_last());
-}
+	if (!$success) {
+	    echo("ERROR: ".error_get_last());
+	}
 	echo("TEST");
 ?>
