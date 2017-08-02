a2dca7c4fc87e932ce0eab6989706d08005ddd90
Cedric Van Pelt <cvp@mobilemyday.com>
2017-08-02 15:04:11 +0200

    test

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
-	mail(
+	$success = mail(
 		'cvp@mobilemyday.com', 
 		'[MMD] test', 
 		'Test' . '<br />' . '<pre>' . 'test2' . '</pre>', 
 		'FROM: mobile my day <test@mobilemyday.com>' . "\n" . 'Content-Type: text/html; charset="utf-8"' . "\n" 
 	);
+if (!$success) {
+    $errorMessage = error_get_last()['message'];
+}
 	echo("TEST");
 ?>
